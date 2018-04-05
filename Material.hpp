040cea03111a7ad034ac37e01e811eb7e94fdb8a
Matthew Dobler <matthew.dobler@bigfishgames.com>
2018-04-04 17:53:25 -0700

    Fixing the blend mode

--- Material.hpp
+++ Material.hpp
@@ -73,7 +73,7 @@ public:
     glUseProgram(ID);
     if (_transparent) {
       glEnable(GL_BLEND);
-      glBlendFunc(GL_ONE, GL_ONE);
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE);
       glBlendEquation(GL_FUNC_ADD);
     }
   }
