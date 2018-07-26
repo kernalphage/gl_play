a483f74e605a6af9b19090d4a0300099f9c7203e
Matt Dobler <doblerman@gmail.com>
2018-07-26 08:49:21 -0700

    Abstracting Materials a little bit to ease reading

--- Material.hpp
+++ Material.hpp
@@ -14,50 +14,50 @@ public:
   bool _transparent;
   // constructor generates the shader on the fly
   // ------------------------------------------------------------------------
-  Material(const char *vertexPath, const char *fragmentPath, bool transparent) {
+  Material(const char *vertexPath, const char *fragmentPath, bool transparent, const char* geomPath = nullptr) {
     _transparent = transparent;
 
-    // 1. retrieve the vertex/fragment source code from filePath
-    std::string vertexCode;
-    std::string fragmentCode;
-    std::ifstream vShaderFile;
-    std::ifstream fShaderFile;
-    // ensure ifstream objects can throw exceptions:
-    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    try {
-      // open files
-      vShaderFile.open(vertexPath);
-      fShaderFile.open(fragmentPath);
-      std::stringstream vShaderStream, fShaderStream;
-      // read file's buffer contents into streams
-      vShaderStream << vShaderFile.rdbuf();
-      fShaderStream << fShaderFile.rdbuf();
-      // close file handlers
-      vShaderFile.close();
-      fShaderFile.close();
-      // convert stream into string
-      vertexCode = vShaderStream.str();
-      fragmentCode = fShaderStream.str();
-    } catch (std::ifstream::failure e) {
-      std::cout<< "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-    }
-    const char *vShaderCode = vertexCode.c_str();
-    const char *fShaderCode = fragmentCode.c_str();
+    auto loadCode = [](const char* path){
+      std::string code;
+      std::ifstream sFile;
+
+      sFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+      // Try
+      sFile.open(path);
+      std::stringstream vShaderStream;
+      vShaderStream << sFile.rdbuf();
+      sFile.close();
+      code = vShaderStream.str();
+      // catch
+      return code;
+    };
+
+    const char *vShaderCode = loadCode(vertexPath).c_str();
+    const char *fShaderCode = loadCode(fragmentPath).c_str();
     // 2. compile shaders
     unsigned int vertex, fragment;
     // vertex shader
     vertex = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex, 1, &vShaderCode, NULL);
     glCompileShader(vertex);
-    checkCompileErrors(vertex, "VERTEX",vertexPath);
+    checkCompileErrors(vertex, "VERTEX", vertexPath);
     // fragment Shader
     fragment = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment, 1, &fShaderCode, NULL);
     glCompileShader(fragment);
     checkCompileErrors(fragment, "FRAGMENT",fragmentPath);
+
     // shader Program
     ID = glCreateProgram();
+
+    if(geomPath != nullptr){
+      const char* gShaderCode = loadCode(geomPath).c_str();
+      unsigned int geom;
+      geom = glCreateShader(GL_GEOMETRY_SHADER);
+      glShaderSource(geom, 1, &gShaderCode, NULL);
+      glAttachShader(ID, geom);
+    }
+
     glAttachShader(ID, vertex);
     glAttachShader(ID, fragment);
     glLinkProgram(ID);
